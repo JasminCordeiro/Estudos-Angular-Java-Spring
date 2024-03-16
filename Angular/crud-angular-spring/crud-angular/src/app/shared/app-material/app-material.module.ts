114ccab5279cdf638cd4af8437872f68a8fd2c29
Jasmin Cordeiro <playgame0159@gmail.com>
2024-03-16 20:12:16 -0300

    Loading

--- Angular/crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
+++ Angular/crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -2,17 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   exports: [
     MatCardModule,
     MatTableModule,
     MatToolbarModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
   ],
-
 })
-export class AppMaterialModule { }
+export class AppMaterialModule {}
