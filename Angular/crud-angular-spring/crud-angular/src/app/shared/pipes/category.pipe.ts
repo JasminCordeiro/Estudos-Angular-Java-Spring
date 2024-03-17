c0e7d38b9624482bb690c07773c5f12369476ab8
Jasmin Cordeiro <playgame0159@gmail.com>
2024-03-16 23:44:26 -0300

    Datas

--- Angular/crud-angular-spring/crud-angular/src/app/shared/pipes/category.pipe.ts
+++ Angular/crud-angular-spring/crud-angular/src/app/shared/pipes/category.pipe.ts
@@ -9,7 +9,7 @@ export class CategoryPipe implements PipeTransform {
     switch(value){
       case 'Front-end' : return 'code'
       case 'Back-end' : return 'computer'
-
+      case 'Mobile' : return 'smartphone'
     }
 
     return 'code';
