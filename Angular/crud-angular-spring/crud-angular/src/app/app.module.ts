4ffc25d5aea9796d044d4f0ec21efb4940fb7f26
Jasmin Cordeiro <playgame0159@gmail.com>
2024-03-16 22:02:31 -0300

    Add Error

--- Angular/crud-angular-spring/crud-angular/src/app/app.module.ts
+++ Angular/crud-angular-spring/crud-angular/src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    HttpClientModule
+    HttpClientModule,
+    MatDialogModule,
+    SharedModule,
+
   ],
   providers: [
     provideAnimationsAsync()
--- Angular/crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
+++ Angular/crud-angular-spring/crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -3,13 +3,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   exports: [
     MatCardModule,
     MatTableModule,
     MatToolbarModule,
     MatProgressSpinnerModule,
+    MatButtonModule
   ],
 })
 export class AppMaterialModule {}
