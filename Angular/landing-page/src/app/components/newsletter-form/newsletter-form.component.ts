82fe2ae26ee16a5adffdba9ba12d2271ebac69c9
Jasmin Cordeiro <playgame0159@gmail.com>
2024-03-31 22:33:32 -0300

    Erro no Http.
    Estou enfrentando um erro de NullInjectorError.

--- Angular/landing-page/src/app/components/newsletter-form/newsletter-form.component.ts
+++ Angular/landing-page/src/app/components/newsletter-form/newsletter-form.component.ts
@@ -2,12 +2,19 @@ import { Component, signal } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NewsletterService } from '../../services/newsletter.service';
+import { HttpClientModule } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'newsletter-form',
   standalone: true,
-  imports: [BtnPrimaryComponent, ReactiveFormsModule],
-  providers:[NewsletterService],
+  imports: [
+    BtnPrimaryComponent,
+    ReactiveFormsModule
+  ],
+  providers: [
+    NewsletterService
+  ],
   templateUrl: './newsletter-form.component.html',
   styleUrl: './newsletter-form.component.scss'
 })
@@ -15,24 +22,23 @@ export class NewsletterFormComponent {
   newsletterForm!: FormGroup;
   loading = signal(false);
 
-  constructor(private service: NewsletterService){
+  constructor(private service: NewsletterService) {
     this.newsletterForm = new FormGroup({
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
-
     });
   }
 
   onSubmit(){
     this.loading.set(true);
-  if(this.newsletterForm.valid){
-    this.service.sendData(this.newsletterForm.value.name, this.newsletterForm.value.email).subscribe({
-      next: ( ) =>{
-        this.newsletterForm.reset();
-        this.loading.set(false);
-      }
-    });
+    if(this.newsletterForm.valid){
+      this.service.sendData(this.newsletterForm.value.name, this.newsletterForm.value.email).subscribe({
+        next: () => {
+          this.newsletterForm.reset();
+          this.loading.set(false);
+        },
+        error: () => this.loading.set(false)
+      })
+    }
   }
-}
-
 }
--- Angular/landing-page/src/app/services/newsletter.service.ts
+++ Angular/landing-page/src/app/services/newsletter.service.ts
@@ -2,24 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-interface NewsletterResponse{
-  message:string
+interface NewsletterResponse {
+  message: string
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsletterService {
-  private endpointUrl = 'https://faed47pcwb7biktidlecuafuty0aegep.lambda-url.us-east-1.on.aws/';
+  private endpointUrl = "https://faed47pcwb7biktidlecuafuty0aegep.lambda-url.us-east-1.on.aws/";
 
+  constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient) {}
+  sendData(name: string, email: string): Observable<NewsletterResponse>{
+    const data = {name, email};
 
-    sendData(name: string, email: string): Observable<NewsletterResponse>{
-      const data = {name, email};
-      return this.http.post<NewsletterResponse>(this.endpointUrl, data);
-
-
-
+    return this.http.post<NewsletterResponse>(this.endpointUrl, data);
   }
 }
