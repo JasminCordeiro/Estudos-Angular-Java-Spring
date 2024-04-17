199de4495cf0ad94e0891309d239a5a1436ee694
Jasmin Cordeiro <playgame0159@gmail.com>
2024-04-16 22:54:00 -0300

    Up

--- Portfolio-Angular/Portfolio/src/app/components/home/home.component.ts
+++ Portfolio-Angular/Portfolio/src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent,NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
