ac5ac1d3af07f2f94e6881e683c3c51cc6824797
Jasmin Cordeiro <playgame0159@gmail.com>
2024-04-20 22:02:15 -0300

    Up

--- Portfolio-Angular/Portfolio/src/app/app.routes.ts
+++ Portfolio-Angular/Portfolio/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { IndexComponent } from './components/index/index.component';
+
 
 export const routes: Routes = [
   {
     path:"",
-    component: HomeComponent
+    component: IndexComponent
+
   }
 ];
